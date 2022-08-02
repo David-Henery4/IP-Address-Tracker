714b4ebf2527430cd6b6a12e5f2b22ef7f9b3383
DizzlyD <david4henery00@hotmail.com>
2022-08-02 15:09:08 +0100

    responsive header ui

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,7 +3,7 @@ import Yes from "../images/icon-arrow.svg"
 const Input = () => {
   return (
     <div className="input-container">
-      <input type="text" className="input__search input" />
+      <input type="text" placeholder='Search for any IP address or domain' className="input__search input" />
       <button className="input__button input">
         <img src={Yes} alt="" />
       </button>
